778a2e3301207362e1a0aa8628b67bd8385891a7
twayn <twayn@ya.ru>
2018-08-01 20:58:16 +0500

    Transfer to Github

---
+++ js/order.call.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function() {
+         		
+         	$("#CntctFrm0-t2usubmit").on("click", function(){
+         
+         
+         		var nameval = document.getElementById('CntctFrm0-t2unameField').value;
+         		var emailval = document.getElementById('CntctFrm0-t2uemailField').value;
+         		var phoneval = document.getElementById('CntctFrm0-t2uphoneField').value;
+         		var themeval = document.getElementById('CntctFrm0-t2usubjectField').value;
+         		var timeval = document.getElementById('CntctFrm0-t2umessageField').value;
+         		var compval = document.getElementById('CntctFrm0-t2ucompanyField').value;
+         
+         		var check = document.getElementById('CntctFrm0-t2usubjectField1').value;
+         		var checkLength = check.length;
+         
+         		var mailvalid = validateEmail(emailval);
+         		var phonevalid = validatePhone(phoneval);
+         		var namevalid = validateName(nameval);
+         
+         		if(namevalid == false) {
+         			$("#CntctFrm0-t2unameField").addClass("error");
+         			$("#correct_name").css('display', 'inline');
+         		}
+         		else if(namevalid == true){
+         			$("#CntctFrm0-t2unameField").removeClass("error");
+         			$("#correct_name").css('display', 'none');
+         		}
+         
+         		if(phonevalid == false) {
+         			$("#CntctFrm0-t2uphoneField").addClass("error");
+         			$("#correct_phone").css('display', 'inline');
+         		}
+         		else if(phonevalid == true){
+         			$("#CntctFrm0-t2uphoneField").removeClass("error");
+         			$("#correct_phone").css('display', 'none');
+         		}
+         		
+         		if(mailvalid == false) {
+         			$("#CntctFrm0-t2uemailField").addClass("error");
+         			$("#correct_email").css('display', 'inline');
+         		}
+         		else if(mailvalid == true){
+         			$("#CntctFrm0-t2uemailField").removeClass("error");
+         			$("#correct_email").css('display', 'none');
+         		}
+         		
+         		
+         		if(mailvalid == true && phonevalid == true && namevalid == true && checkLength == 0) {
+         			console.log("Имя: ", nameval);
+         			console.log("Телефон: ", phoneval);
+         			console.log("E-mail: ", emailval);
+         			var msgval = "Название компании: " + compval + ". " + "Тема: " + themeval + ". " + "Удобное время: " + timeval;
+         			console.log(msgval);
+         
+         			$.ajax({
+         						url: 'http://www.company.su/ajax.html',
+         						data: {
+         							name: nameval,
+         		                	phone: phoneval,
+         		                	email: emailval,
+         		                	msg: msgval
+         						},
+         						dataType: 'jsonp',
+         						jsonp : 'callback',
+         						headers: {'X-Requested-With': 'XMLHttpRequest'},
+         						timeout: 5000,
+         						crossDomain: true,
+         					}).done(function(response){
+         						console.log("Done");
+         		                //console.log(response);
+         
+         					}).fail(function(error){
+         						console.log("Fail");
+         		                //console.log(error.statusText);
+         					});
+         
+         			$("#send_text").css('display', 'inline');
+         			$("#CntctFrm0-t2usubmit").replaceWith($("#send_text"));
+         
+         		}
+         		
+         	});
+         });
---
+++ js/start.work.js
@@ -0,0 +1,99 @@
+﻿ function validateEmail(email) { 
+         	var reg = /^([A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+@[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*\.[A-Za-z]{2,6}$/;
+         	return reg.test(email);
+         }
+         function validatePhone(phone) { 
+         	var reg = /^\d[\d\(\)\ -]{4,14}\d$/;	//Только цифры, пробелы, дефисы от 4 до 14
+         	return reg.test(phone);
+         }
+         function validateName(name){ 
+         	var reg = /^[a-z A-Z А-Я а-я Ёё]{3,40}$/;	//только буквы от 3 до 40
+         	return reg.test(name);
+         }
+         
+         
+         $(document).ready(function() {
+         	$(".modalbox").fancybox();
+         	$("#contact").submit(function() { return false; });
+         
+         	
+         	$("#send").on("click", function(){
+         		
+         		var workplval = document.getElementById('workplace').value;
+         		var nameval = document.getElementById('forename').value;
+         		var emailval  = document.getElementById('email').value;
+         		var phoneval = document.getElementById('phone').value;
+         		var msgval    = document.getElementById('msg').value;
+         		var comvar = document.getElementById('company').value;
+         
+         		var check = document.getElementById('Name').value;
+         		var checkLength = check.length;
+         
+         		var mailvalid = validateEmail(emailval);
+         		var phonevalid = validatePhone(phoneval);
+         		var namevalid = validateName(nameval);
+         
+         		
+         
+         		if(namevalid == false) {
+         			$("#forename").addClass("error");
+         			$("#corr_name").css('display', 'inline');
+         		}
+         		else if(namevalid == true){
+         			$("#forename").removeClass("error");
+         			$("#corr_name").css('display', 'none');
+         		}
+         
+         
+         		if(phonevalid == false) {
+         			$("#phone").addClass("error");
+         			$("#corr_phone").css('display', 'inline');
+         		}
+         		else if(phonevalid == true){
+         			$("#phone").removeClass("error");
+         			$("#corr_phone").css('display', 'none');
+         		}
+         		
+         		if(mailvalid == false) {
+         			$("#email").addClass("error");
+         			$("#corr_email").css('display', 'inline');
+         		}
+         		else if(mailvalid == true){
+         			$("#email").removeClass("error");
+         			$("#corr_email").css('display', 'none');
+         		}
+         		
+         		
+         		if(mailvalid == true && phonevalid == true && namevalid == true && checkLength == 0) {
+         			console.log("Имя: ", nameval);
+         			console.log("Телефон: ", phoneval);
+         			console.log("E-mail: ", emailval);
+         			var msgval = "Число рабочих мест: " + workplval + ". Компания: " + comvar + ". " + "Сообщение: " + msgval;
+         			console.log(msgval);
+         
+         				$.ajax({
+         					url: 'http://www.asgor.su/ajax.html',
+         					data: {
+         						name: nameval,
+         	                	phone: phoneval,
+         	                	email: emailval,
+         	                	msg: msgval
+         					},
+         					dataType: 'jsonp',
+         					jsonp : 'callback',
+         					headers: {'X-Requested-With': 'XMLHttpRequest'},
+         					timeout: 5000,
+         					crossDomain: true,
+         				}).done(function(response){
+         					console.log("Done");
+         	                //console.log(response);
+         
+         				}).fail(function(error){
+         					console.log("Fail");
+         	                //console.log(error.statusText);
+         				});
+         
+         				$("#send").replaceWith("<em>Сообщение отправлено</em>");// скрывается кнопка отправки
+         		}
+         	});
+         });
