90b0918ff511f25c15146e9a659916ff3c0fa6c8
twayn <twayn@ya.ru>
2018-08-05 16:41:16 +0500

    Java script send message methods are fixed.

--- js/order.call.js
+++ js/order.call.js
@@ -1,51 +1,41 @@
 ﻿$(document).ready(function() {
-	$("#CntctFrm0-t2usubmit").on("click", function(){
+    $("#order-call").on("submit", function(){
+        const nameval = document.getElementById('input1').value;
+        const compval = document.getElementById('input2').value;
+        const emailval = document.getElementById('input2').value;
+        const phoneval = document.getElementById('input3').value;
+        const themeval = document.getElementById('input4').value;
+        const timeval = document.getElementById('textarea1').value;
 
-        const nameval = document.getElementById('CntctFrm0-t2unameField').value;
-        const emailval = document.getElementById('CntctFrm0-t2uemailField').value;
-        const phoneval = document.getElementById('CntctFrm0-t2uphoneField').value;
-        const themeval = document.getElementById('CntctFrm0-t2usubjectField').value;
-        const timeval = document.getElementById('CntctFrm0-t2umessageField').value;
-        const compval = document.getElementById('CntctFrm0-t2ucompanyField').value;
+		console.log("Имя: ", nameval);
+		console.log("Телефон: ", phoneval);
+		console.log("E-mail: ", emailval);
+        const msgval = "Название компании: " + compval + ". " + "Тема: " + themeval + ". " + "Удобное время: " + timeval;
+        console.log(msgval);
 
-        const check = document.getElementById('CntctFrm0-t2usubjectField1').value;
-        const checkLength = check.length;
+		$.ajax({
+				url: 'http://www.company.su/ajax.html',
+				data: {
+					name: nameval,
+					phone: phoneval,
+					email: emailval,
+					msg: msgval
+				},
+				dataType: 'jsonp',
+				jsonp : 'callback',
+				headers: {'X-Requested-With': 'XMLHttpRequest'},
+				timeout: 5000,
+				crossDomain: true,
+			}).done(function(response){
+				console.log("Done");
+				//console.log(response);
 
-        const mailvalid = validateEmail(emailval);
-        const phonevalid = validatePhone(phoneval);
-        const namevalid = validateName(nameval);
+			}).fail(function(error){
+				console.log("Fail");
+				//console.log(error.statusText);
+			});
 
-		if(mailvalid === true && phonevalid === true && namevalid === true && checkLength === 0) {
-			console.log("Имя: ", nameval);
-			console.log("Телефон: ", phoneval);
-			console.log("E-mail: ", emailval);
-			var msgval = "Название компании: " + compval + ". " + "Тема: " + themeval + ". " + "Удобное время: " + timeval;
-			console.log(msgval);
-
-			$.ajax({
-						url: 'http://www.company.su/ajax.html',
-						data: {
-							name: nameval,
-							phone: phoneval,
-							email: emailval,
-							msg: msgval
-						},
-						dataType: 'jsonp',
-						jsonp : 'callback',
-						headers: {'X-Requested-With': 'XMLHttpRequest'},
-						timeout: 5000,
-						crossDomain: true,
-					}).done(function(response){
-						console.log("Done");
-						//console.log(response);
-
-					}).fail(function(error){
-						console.log("Fail");
-						//console.log(error.statusText);
-					});
-
-			$("#send_text").css('display', 'inline');
-			$("#CntctFrm0-t2usubmit").replaceWith($("#send_text"));
-		}
+        $("#order-call-button").replaceWith("<em>Сообщение отправлено</em>");// скрывается кнопка отправки
+        return false;
 	});
  });
--- js/start.work.js
+++ js/start.work.js
@@ -1,19 +1,5 @@
-﻿ function validateEmail(email) {
-     const reg = /^([A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+@[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*\.[A-Za-z]{2,6}$/;
-     return reg.test(email);
- }
- function validatePhone(phone) {
-     const reg = /^\d[\d() -]{4,14}\d$/;	//Только цифры, пробелы, дефисы от 4 до 14
-     return reg.test(phone);
- }
- function validateName(name){
-     const reg = /^[a-zA-ZА-Яа-яЁё ]{3,40}$/;	//только буквы от 3 до 40
-     return reg.test(name);
- }
-
- $(document).ready(function() {
-    $("#contact").submit(function() { return false; });//TODO interesting
-    $("#send").on("click", function(){
+﻿ $(document).ready(function() {
+    $("#contact").on("submit", function(){
         const workplval = document.getElementById('workplace').value;
         const nameval = document.getElementById('forename').value;
         const emailval = document.getElementById('email').value;
@@ -21,43 +7,36 @@
         const msgval = document.getElementById('msg').value;
         const comvar = document.getElementById('company').value;
 
-        const check = document.getElementById('Name').value;
-        const checkLength = check.length;
+        console.log("Имя: ", nameval);
+        console.log("Телефон: ", phoneval);
+        console.log("E-mail: ", emailval);
+        const resultMsg = "Число рабочих мест: " + workplval + ". Компания: " + comvar + ". " + "Сообщение: " + msgval;
+        console.log(resultMsg);
 
-        const mailvalid = validateEmail(emailval);
-        const phonevalid = validatePhone(phoneval);
-        const namevalid = validateName(nameval);
+        $.ajax({
+            url: 'http://www.company.su/ajax.html',
+            data: {
+                name: nameval,
+                phone: phoneval,
+                email: emailval,
+                msg: resultMsg
+            },
+            dataType: 'jsonp',
+            jsonp : 'callback',
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            timeout: 5000,
+            crossDomain: true,
+        }).done(function(response){
+            console.log("Done");
+            //console.log(response);
 
-        if(mailvalid === true && phonevalid === true && namevalid === true && checkLength === 0) {
-            console.log("Имя: ", nameval);
-            console.log("Телефон: ", phoneval);
-            console.log("E-mail: ", emailval);
-            const resultMsg = "Число рабочих мест: " + workplval + ". Компания: " + comvar + ". " + "Сообщение: " + msgval;
-            console.log(resultMsg);
+        }).fail(function(error){
+            console.log("Fail");
+            //console.log(error.statusText);
+        });
 
-                $.ajax({
-                    url: 'http://www.asgor.su/ajax.html',
-                    data: {
-                        name: nameval,
-                        phone: phoneval,
-                        email: emailval,
-                        msg: resultMsg
-                    },
-                    dataType: 'jsonp',
-                    jsonp : 'callback',
-                    headers: {'X-Requested-With': 'XMLHttpRequest'},
-                    timeout: 5000,
-                    crossDomain: true,
-                }).done(function(response){
-                    console.log("Done");
-                    //console.log(response);
+        $("#send").replaceWith("<em>Сообщение отправлено</em>");// скрывается кнопка отправки
 
-                }).fail(function(error){
-                    console.log("Fail");
-                    //console.log(error.statusText);
-                });
-
-                $("#send").replaceWith("<em>Сообщение отправлено</em>");// скрывается кнопка отправки
-        }
+        return false;
     });
  });
